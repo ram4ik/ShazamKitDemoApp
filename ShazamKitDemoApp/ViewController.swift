42d68b9e9a5a0ccb7c971f6e5720c07af3f562c3
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-06-14 14:23:37 +0300

    init

--- ShazamKitDemoApp/ViewController.swift
+++ ShazamKitDemoApp/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import ShazamKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SHSessionDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        recognizeSong()
     }
 
-
+    private func recognizeSong() {
+        let session = SHSession()
+        session.delegate = self
+        do {
+            guard let url = Bundle.main.url(forResource: "song", withExtension: "mp3") else { return }
+            let file = try AVAudioFile(forReading: url)
+            guard let buffer = AVAudioPCMBuffer(pcmFormat: file.processingFormat, frameCapacity: AVAudioFrameCount(file.length / 30)) else { return }
+            try file.read(into: buffer)
+            let generator = SHSignatureGenerator()
+            try generator.append(buffer, at: nil)
+            let signature = generator.signature()
+            session.match(signature)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func session(_ session: SHSession, didFind match: SHMatch) {
+        let items = match.mediaItems
+        items.forEach { item in
+            print(item.title ?? "n/a")
+            print(item.artist ?? "n/a")
+            print(item.artworkURL?.absoluteURL ?? "n/a")
+        }
+    }
+    
+    func session(_ session: SHSession, didNotFindMatchFor signature: SHSignature, error: Error?) {
+        if let error = error {
+            print(error)
+        }
+    }
 }
 
